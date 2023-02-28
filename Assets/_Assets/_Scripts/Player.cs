7af8287a0a2776d735b5890163be5e293e10da04
ipubrich <38372852+ipubrich@users.noreply.github.com>
2023-02-28 16:53:36 +0000

    raycast collision

--- Assets/_Assets/_Scripts/Player.cs
+++ Assets/_Assets/_Scripts/Player.cs
@@ -11,9 +11,49 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        
         Vector3 moveDir = new(inputVector.x, 0f, inputVector.y);
-        transform.position += moveSpeed * Time.deltaTime * moveDir;
+
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            // cannot move towards moveDir - hug wall
+
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                // can only move on X axis 
+                moveDir = moveDirX;
+            } else
+            {
+                // Cannot move on Z axis
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+               
+                if (canMove)
+                {
+                    // can only move on Z axis 
+                    moveDir = moveDirZ;
+
+                }
+                else
+                {
+                    // cannot move anywhere
+                }
+
+
+            }
+        }
+            if (canMove)
+        {
+            transform.position += moveSpeed * Time.deltaTime * moveDir;
+        }
 
         isWalking = moveDir != Vector3.zero;
 
