9b23b297ebc6184451d9927ddf475beaf236ab90
ipubrich <38372852+ipubrich@users.noreply.github.com>
2023-03-01 21:08:42 +0000

    refactor player and kitchen counter to use interface and move object between them and player

---
+++ Assets/_Assets/_Scripts/IKitchenObjectParent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IKitchenObjectParent 
+{
+    // define functions / properties as a contract. Implementations must done in the class for ALL definitions and are can vary
+    
+    public Transform GetKitchenObjectFollowTransform();
+    public void SetKitchenObject(KitchenObject kitchenObject);
+    public KitchenObject GetKitchenObject();
+    public void ClearKitchenObject();
+    public bool HasKitchenObject();
+}
