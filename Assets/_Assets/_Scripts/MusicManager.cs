e66f97ef1ff87ee10de8993e6749daa31be66269
ipubrich <38372852+ipubrich@users.noreply.github.com>
2023-03-21 21:30:07 +0000

    Options menu, audio levels, saving playerprefs to keyvalues

---
+++ Assets/_Assets/_Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private const string PLAYER_PREFS_MUSIC_VOLUME = "MusicVolume";
+    // in order to call this from the Options UI we need a singleton
+    public static MusicManager Instance { get; private set; }
+
+    private float volume = .3f;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_MUSIC_VOLUME, .3f);
+        Instance = this;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = volume;
+    }
+    public void ChangeVolume()
+    {
+        volume += .1f;
+        //volume = volume % 1.1f;
+        if (volume > 1f)
+        {
+            volume = 0f;
+        }
+        audioSource.volume = volume;
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_MUSIC_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+    public float GetVolume()
+    {
+        return volume;
+    }
+}
--- Assets/_Assets/_Scripts/SoundManager.cs
+++ Assets/_Assets/_Scripts/SoundManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
     public static SoundManager Instance { get; private set; } // Enable instances of this to be called elsewhere
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    private float volume = 1f;
     private void Awake()
     {
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
         Instance = this;
     }
     private void Start()
@@ -63,9 +66,9 @@ public class SoundManager : MonoBehaviour
         PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
     }
 
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
     }
 
     // just to be different for PlayerSounds
@@ -73,6 +76,23 @@ public class SoundManager : MonoBehaviour
     public void PlayFootStepsSound(Vector3 position, float volume)
     {
         PlaySound(audioClipRefsSO.footstep, position, volume);
+    }
+
+    public void ChangeVolume()
+    {
+        volume += .1f;
+        //volume = volume % 1.1f;
+        if (volume > 1f)
+        {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+    public float GetVolume()
+    {
+        return volume;
     }
 
 }
